785b7dca999f2dd339a5abe661f8efc11357ab8b
Joao Gamboias <joao.gamboias@gmail.com>
2022-03-02 21:41:10 +0000

    api changed to fastapi, working for description only

--- app.py
+++ app.py
@@ -1,22 +1,39 @@
-from flask import Flask
-from flask_restful import Resource, Api, reqparse
+# Importing Necessary modules
+from fastapi import FastAPI
+import uvicorn
+from joblib import load
+import numpy as np
 import pandas as pd
-import ast
 
-app = Flask(__name__)
-api = Api(app)
+ 
+# Declaring our FastAPI instance
+app = FastAPI()
+ 
+# Defining path operation for root endpoint
+@app.get('/')
+def main():
+    return {'message': 'Welcome to GeeksforGeeks (basic)!'}
+ 
+# # Defining path operation for /name endpoint
+# @app.get('/{name}')
+# def hello_name(name : str):
+#     # Defining a function that takes only string as input and output the
+#     # following message.
+#     return {'message': f'Welcome to GeeksforGeeks!, {name}'}
 
-def classify_product(description):
-    # load model
-    text_clf = load('text_clf.joblib') 
-    predicted = text_clf.predict(pd.Series(test_data))
+
+# Defining path operation for /name endpoint
+@app.get('/{description}')
+def hello_name(description : str):
+    # Defining a function that takes only string as input and output the
+    # following message.
+    
+    clf = load('text_clf.joblib') 
+    predicted = clf.predict(pd.Series(description))
+    
+    result = {'category': list(predicted)}
     
     
+    return result
 
-    return {'predicted': predicted}, 200  # return data and 200 OK
 
-
-api.add_resource(Users, '/classify_product')  # add endpoints
-
-if __name__ == '__main__':
-    app.run()  # run our Flask app
